c455c0d718340477c79d696f92a17c300ec0152d
Matheus Gomes <m.gomescontato@gmail.com>
2020-05-06 17:19:33 -0300

    Estrutura inicial projeto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Home() => "Seja Bem vindo!";
+    }
+}
